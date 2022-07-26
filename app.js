77da085d375470f2c1ed8657b7c1629c2a81c332
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-07-26 14:21:48 -0400

    added html docs

---
+++ app.js
@@ -0,0 +1,17 @@
+// $( () => {
+  
+// const $btn = $('#btn');
+    
+// const addText = () => {
+//     $('body').append("Thank you for visiting my page");
+// }
+// $btn.on('click', addText);
+
+
+
+// $("p").click(function(){
+//     alert("The paragraph was clicked.");
+//   });
+
+
+//   });
