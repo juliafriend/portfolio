139effceea77b0b8f8d1d7d04c3eed2959db928c
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-07-27 14:27:09 -0400

    carousel working

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-$( () => {
-  
+$( () => {    
+/////
+$("p").click(function(){
+    alert("The paragraph was clicked.");
+
+  });
+/////
 const $button = $('#button');
-    
 const addMessage = () => {
     $('body').append("Thank you for visiting! Check back soon for updated content");
 }
 $button.on('click', addMessage);
 
-
-
-// $("p").click(function(){
-//     alert("The paragraph was clicked.");
-//   });
-
+//////////Carousel//////////
+let currentImgIndex = 0
+let numOfImages = $('.carousel-imgs').children().length - 1
+$('.next').on('click', () => {
+    $('.carousel-imgs').children().eq(currentImgIndex).css('display', 'none')
+    if(currentImgIndex < numOfImages) {
+        currentImgIndex ++
+       } else {
+        currentImgIndex = 0
+       }
+    $('.carousel-imgs').children().eq(currentImgIndex).css('display', 'block')
+})
+$('.previous').on('click', () => {
+    $('.carousel-imgs').children().eq(currentImgIndex).css('display', 'none')
+    if(currentImgIndex > 0) {
+        currentImgIndex--
+    } else {
+        currentImgIndex = numOfImages
+    }
+    $('.carousel-imgs').children().eq(currentImgIndex).css('display', 'block')
+})
 
   });
